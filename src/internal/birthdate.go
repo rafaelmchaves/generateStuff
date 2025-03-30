1a3e99df102d473754bb56f688a6b670910095a6
Rafael Marques Chaves <rafaelmchaves@gmail.com>
2025-03-30 12:22:05 -0300

    Create a generate birthdate thinking in a probability passing a age slot list

--- src/internal/birthdate.go
+++ src/internal/birthdate.go
@@ -40,3 +40,57 @@ func daysInMonth(month time.Month, year int) int {
 		return 31
 	}
 }
+
+// Generate birthdate thinking in slots and odds(e.g should generate less people who is more than 80 years old)
+func GenerateBirthdateUsingOddsSlot(ageSlots []*AgeSlot) string {
+	maxAge := 110
+
+	rand.Seed(time.Now().UnixNano())
+	tries := 0
+	selectedSlot := -1
+	randomAge := -1
+	for selectedSlot == -1 && tries < 50 {
+		randomAge = rand.Intn(maxAge)
+		for i, slot := range ageSlots {
+			if randomAge >= slot.MinAge && randomAge < slot.MaxAge && slot.LeftQuantity > 0 {
+				selectedSlot = i
+				slot.LeftQuantity -= 1
+				tries = 0
+				break
+			}
+
+		}
+
+		tries++
+	}
+
+	if selectedSlot != -1 {
+		return generateBirthdateByAge(randomAge)
+	}
+
+	fmt.Println("no one to be created anymore")
+	return ""
+}
+
+func generateBirthdateByAge(age int) string {
+	rand.Seed(time.Now().UnixNano())
+
+	// Get the current time
+	now := time.Now()
+
+	// Calculate the birth year
+	birthYear := now.Year() - age
+
+	month := time.Month(rand.Intn(12) + 1)
+
+	dayOptions := daysInMonth(month, birthYear)
+	day := rand.Intn(dayOptions) + 1
+
+	return fmt.Sprintf("%02d/%02d/%d", day, time.Month(month), birthYear)
+}
+
+type AgeSlot struct {
+	MinAge       int
+	MaxAge       int
+	LeftQuantity int64 // quantity of people in this slot left to be created
+}
--- src/main.go
+++ src/main.go
@@ -1,10 +1,52 @@
 package main
 
-import "github.com/rafaelmchaves/generatestuff/src/internal"
+import (
+	"fmt"
+
+	"github.com/rafaelmchaves/generatestuff/src/internal"
+)
 
 func main() {
 
 	println("Generate stuff - application")
 	birthdate := internal.GenerateBirthdate()
 	println("birthdate", birthdate)
+
+	ageSlots := []*internal.AgeSlot{
+		{MinAge: 0, MaxAge: 4, LeftQuantity: 500},
+		{MinAge: 5, MaxAge: 9, LeftQuantity: 400},
+		{MinAge: 10, MaxAge: 14, LeftQuantity: 600},
+		{MinAge: 15, MaxAge: 19, LeftQuantity: 800},
+		{MinAge: 20, MaxAge: 24, LeftQuantity: 900},
+		{MinAge: 25, MaxAge: 29, LeftQuantity: 1000},
+		{MinAge: 30, MaxAge: 34, LeftQuantity: 1100},
+		{MinAge: 35, MaxAge: 39, LeftQuantity: 1200},
+		{MinAge: 40, MaxAge: 44, LeftQuantity: 900},
+		{MinAge: 45, MaxAge: 49, LeftQuantity: 700},
+		{MinAge: 50, MaxAge: 54, LeftQuantity: 500},
+		{MinAge: 55, MaxAge: 59, LeftQuantity: 300},
+		{MinAge: 60, MaxAge: 64, LeftQuantity: 200},
+		{MinAge: 65, MaxAge: 69, LeftQuantity: 100},
+		{MinAge: 70, MaxAge: 74, LeftQuantity: 50},
+		{MinAge: 75, MaxAge: 79, LeftQuantity: 20},
+		{MinAge: 80, MaxAge: 84, LeftQuantity: 10},
+		{MinAge: 85, MaxAge: 89, LeftQuantity: 5},
+		{MinAge: 90, MaxAge: 94, LeftQuantity: 2},
+		{MinAge: 95, MaxAge: 100, LeftQuantity: 1},
+	}
+
+	people := 1
+	for people <= 50000 {
+		birthdate := internal.GenerateBirthdateUsingOddsSlot(ageSlots)
+		fmt.Println("person:", people, "birthdate:", birthdate)
+		if birthdate == "" {
+			break
+		}
+		people++
+	}
+
+	for _, slot := range ageSlots {
+		fmt.Printf("MinAge: %d, MaxAge: %d, LeftQuantity: %d\n", slot.MinAge, slot.MaxAge, slot.LeftQuantity)
+	}
+
 }
